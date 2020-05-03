622767865457f89d4e0aecc29195281f1c6d6e2c
ubuntu <peter.todorov@live.com>
2020-05-03 12:52:49 +0300

    Remove 'getBallbacks' function from 'kaggle_test.py'

--- kaggle_test.py
+++ kaggle_test.py
@@ -200,42 +200,14 @@ class DataGenerator:
             yield X_batch, y_batch
 
 
-def getCallbacks(patience, save_root, batch_size):
-    # Define saving file paths
-    model_file_name = "{}/model.h5".format(save_root)
-    csv_log_file_name = "{}/logs.csv".format(save_root)
-
-    # Create folders if do not exist
-    if not os.path.isdir(save_root):
-        os.makedirs(save_root)
-
-    # Define callbacks
-    early_stopping = EarlyStopping(patience=patience)
-    checkpointer = ModelCheckpoint(
-        model_file_name, verbose=1, save_best_only=True)
-    reduce_learning_rate = ReduceLROnPlateau(
-        factor=0.3, patience=4, min_lr=1e-8)
-    csv_logger = CSVLogger(csv_log_file_name, separator=';')
-    tensor_board = TensorBoard(
-        log_dir=save_root, write_graph=False, batch_size=batch_size)
-    callbacks = [
-        early_stopping,
-        checkpointer,
-        reduce_learning_rate,
-        csv_logger,
-        tensor_board]
-    return callbacks
-
-
 def loadModel(load_pretrained_model=True, model_root="models"):
-    if load_pretrained_model:
-        latest_model = sorted(glob.glob(model_root + "/*/*.h5"))[-1]
-        model = load_model(
-            latest_model,
-            custom_objects={
-                "tf": tf
-            })
-        print("Loaded model: {}".format(latest_model))
+    latest_model = sorted(glob.glob(model_root + "/*/*.h5"))[-1]
+    model = load_model(
+        latest_model,
+        custom_objects={
+            "tf": tf
+        })
+    print("Loaded model: {}".format(latest_model))
     print("Number of model parameters: {:,}".format(model.count_params()))
     return model
 
@@ -272,6 +244,8 @@ def main():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.Session(config=config)
+
+    # Test
     test()
 
 
