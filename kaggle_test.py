2daca81199a2fa7f5ae09790b1c8d495ff72985a
ubuntu <peter.todorov@live.com>
2020-05-04 22:50:16 +0300

    Update data generator to 'kaggle_test.py'

--- kaggle_test.py
+++ kaggle_test.py
@@ -16,6 +16,7 @@ import math
 import random
 import pandas as pd
 import os
+import cv2
 from openslide import OpenSlide
 
 
@@ -27,12 +28,13 @@ TEST_DIR = os.path.join(ROOT, "test_images")
 BATCH_SIZE = 8
 PATCH_SIZE = 64
 PATCHES_PER_IMAGE = 16
+CONCATENATE_PATCHES = True
 
 
 class DataGenerator:
     def __init__(
             self, data_directory, batch_size, patch_size, patches_per_image=1,
-            train_valid_split=None):
+            train_valid_split=None, concatenate_patches=False):
         self.__available_indices = []
         self.__latest_used_indices = []
         self.__image_names = []
@@ -45,6 +47,7 @@ class DataGenerator:
         self.__sample_split_index = None
         self.__data_directory = data_directory
         self.__labels = None
+        self.__concatenate_patches = concatenate_patches
 
         self.__defineFileNames()
 
@@ -156,6 +159,18 @@ class DataGenerator:
             else:
                 self.__labels = all_labels[:self.__sample_split_index]
 
+    def __createSquarePatches(self, batch):
+        batch = list(np.moveaxis(np.array(batch), 0, 1))
+        patches_per_row = int(math.sqrt(self.__patches_per_image))
+        concat_batch = []
+        for patches in batch:
+            hconcat_patches = []
+            for i in range(patches_per_row):
+                hconcat_patches.append(cv2.hconcat(patches[:patches_per_row]))
+                patches = patches[patches_per_row:]
+            concat_batch.append(cv2.vconcat(hconcat_patches))
+        return np.array(concat_batch)
+
     def getImageGeneratorAndNames(
             self, normalize=False, shuffle=True):
         self.__shuffle = shuffle
@@ -172,7 +187,10 @@ class DataGenerator:
                 for i in self.__latest_used_indices]), 0, 1)
             if normalize:
                 images = self.normalizeArray(np.array(images))
-            yield list(images), image_names
+            images = list(images)
+            if self.__concatenate_patches:
+                images = self.__createSquarePatches(images)
+            yield images, image_names
 
     def normalizeArray(self, data_array, max_value=255):
         return ((data_array / max_value - 0.5) * 2).astype(np.float32)
@@ -217,7 +235,8 @@ def test():
     submission_file = pd.read_csv(os.path.join(ROOT, "sample_submission.csv"))
     model = loadModel()
     test_generator = DataGenerator(
-        TEST_DIR, BATCH_SIZE, PATCH_SIZE, PATCHES_PER_IMAGE)
+        TEST_DIR, BATCH_SIZE, PATCH_SIZE, PATCHES_PER_IMAGE,
+        concatenate_patches=CONCATENATE_PATCHES)
     test_batch_generator = test_generator.getImageGeneratorAndNames(
         normalize=True, shuffle=False)
     number_of_batches = test_generator.numberOfBatchesPerEpoch()
