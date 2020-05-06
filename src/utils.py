86547841b67c491233544c49bd795db4b0c076b6
ubuntu <peter.todorov@live.com>
2020-05-06 10:30:29 +0300

    Remove cohen kappa implementations

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,6 @@ from tensorflow.keras.callbacks import \
     CSVLogger, EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 
 import os
-from sklearn.metrics import confusion_matrix
 import math
 import numpy as np
 
@@ -32,40 +31,6 @@ def getCallbacks(patience, save_root, batch_size):
         csv_logger,
         tensor_board]
     return callbacks
-
-
-def quadratic_kappa(actuals, preds, N=5):
-    """
-    This function calculates the Quadratic Kappa Metric used for Evaluation in
-    the PetFinder competition at Kaggle. It returns the Quadratic Weighted
-    Kappa metric score between the actual and the predicted values of adoption
-    rating.
-    """
-    w = np.zeros((N, N))
-    O = confusion_matrix(actuals, preds)
-    for i in range(len(w)):
-        for j in range(len(w)):
-            w[i][j] = float(((i-j)**2)/(N-1)**2)
-
-    act_hist = np.zeros([N])
-    for item in actuals:
-        act_hist[item] += 1
-
-    pred_hist = np.zeros([N])
-    for item in preds:
-        pred_hist[item] += 1
-
-    E = np.outer(act_hist, pred_hist)
-    E = E/E.sum()
-    O = O/O.sum()
-
-    num = 0
-    den = 0
-    for i in range(len(w)):
-        for j in range(len(w)):
-            num += w[i][j]*O[i][j]
-            den += w[i][j]*E[i][j]
-    return (1 - (num/den))
 
 
 def getNumberOfSteps(data_directory, batch_size):
