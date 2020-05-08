3babb6be75d6ab5521019f096503ff6a683f77d1
ubuntu <peter.todorov@live.com>
2020-05-08 13:40:41 +0300

    Update callback parameters

--- src/utils.py
+++ src/utils.py
@@ -18,9 +18,10 @@ def getCallbacks(patience, save_root, batch_size):
     # Define callbacks
     early_stopping = EarlyStopping(patience=patience)
     checkpointer = ModelCheckpoint(
-        model_file_name, verbose=1, save_best_only=True)
+        model_file_name, verbose=1, save_best_only=True,
+        save_weights_only=False)
     reduce_learning_rate = ReduceLROnPlateau(
-        factor=0.3, patience=2, min_lr=1e-8)
+        factor=0.3, patience=5, min_lr=1e-8)
     csv_logger = CSVLogger(csv_log_file_name, separator=';')
     tensor_board = TensorBoard(
         log_dir=save_root, write_graph=False, batch_size=batch_size)
