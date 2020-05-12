c7c460dfcfb9a95fb6a4fb55649d23a1732df591
ubuntu <peter.todorov@live.com>
2020-05-12 08:45:03 +0300

    Compute kappa in training

--- train_keras.py
+++ train_keras.py
@@ -13,25 +13,24 @@ import time
 # Project files
 from src.image_generator import DataGenerator
 from src.network import net
-from src.utils import getCallbacks
+from src.utils import getCallbacks, quadraticKappa
 
 
 # Data paths
 ROOT = os.path.realpath("../input/prostate-cancer-grade-assessment")
-TRAIN_X_DIR = os.path.join(ROOT, "train_images")
+TRAIN_X_DIR = os.path.join(ROOT, "train_patches_256_4x4_low_res")
 TRAIN_Y_DIR = os.path.join(ROOT, "train.csv")
-VALID_X_DIR = os.path.join(ROOT, "valid_images")
+VALID_X_DIR = os.path.join(ROOT, "valid_patches_256_4x4_low_res")
 VALID_Y_DIR = os.path.join(ROOT, "valid.csv")
 
 # Model parameters
 LOAD_MODEL = False
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 PATCH_SIZE = 64
 PATCHES_PER_IMAGE = 16
 EPOCHS = 1000
 PATIENCE = 10
 LEARNING_RATE = 1e-4
-CONCATENATE_PATCHES = True
 
 PROGRAM_TIME_STAMP = time.strftime("%Y-%m-%d_%H%M%S")
 
@@ -46,17 +45,15 @@ def loadModel(load_pretrained_model=True, model_root="models"):
             })
         print("Loaded model: {}".format(latest_model))
     else:
-        input_shape = (PATCH_SIZE, PATCH_SIZE, 3)
-        if CONCATENATE_PATCHES:
-            input_side = PATCH_SIZE * int(math.sqrt(PATCHES_PER_IMAGE))
-            input_shape = (input_side, input_side, 3)
-        model = net(input_shape, PATCHES_PER_IMAGE)
+        input_side = PATCH_SIZE * int(math.sqrt(PATCHES_PER_IMAGE))
+        input_shape = (input_side, input_side, 3)
+        model = net(input_shape)
 
         # Compile model
         model.compile(
             optimizer=Adam(LEARNING_RATE),
             loss="categorical_crossentropy",
-            metrics=["accuracy"])
+            metrics=["accuracy", quadraticKappa])
     print("Number of model parameters: {:,}".format(model.count_params()))
     return model
 
@@ -68,13 +65,12 @@ def train():
 
     # Load data generators
     train_generator = DataGenerator(
-        TRAIN_X_DIR, BATCH_SIZE, PATCH_SIZE, PATCHES_PER_IMAGE,
-        concatenate_patches=CONCATENATE_PATCHES, normalize=True, rotate=True)
+        TRAIN_X_DIR, BATCH_SIZE, PATCH_SIZE, PATCHES_PER_IMAGE, normalize=True,
+        rotate=True)
     train_batch_generator = train_generator.trainImagesAndLabels(TRAIN_Y_DIR)
     number_of_train_batches = train_generator.numberOfBatchesPerEpoch()
     valid_generator = DataGenerator(
-        VALID_X_DIR, BATCH_SIZE, PATCH_SIZE, PATCHES_PER_IMAGE,
-        concatenate_patches=CONCATENATE_PATCHES, normalize=True)
+        VALID_X_DIR, BATCH_SIZE, PATCH_SIZE, PATCHES_PER_IMAGE, normalize=True)
     valid_batch_generator = valid_generator.trainImagesAndLabels(VALID_Y_DIR)
     number_of_valid_batches = valid_generator.numberOfBatchesPerEpoch()
 
@@ -83,8 +79,10 @@ def train():
 
     # Start training
     history = model.fit_generator(
-        train_batch_generator, steps_per_epoch=number_of_train_batches,
-        epochs=EPOCHS, validation_data=valid_batch_generator,
+        train_batch_generator,
+        epochs=EPOCHS,
+        steps_per_epoch=number_of_train_batches,
+        validation_data=valid_batch_generator,
         validation_steps=number_of_valid_batches,
         callbacks=callbacks)
     print("Model saved to: '{}'".format(save_root))
