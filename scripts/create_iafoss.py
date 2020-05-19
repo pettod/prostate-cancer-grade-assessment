545728f0515aeea6dde9f1e318190c9d9f615ddf
ubuntu <peter.todorov@live.com>
2020-05-19 21:18:14 +0300

    Save Iafoss crop not to zip

--- scripts/create_iafoss.py
+++ scripts/create_iafoss.py
@@ -1,59 +1,43 @@
 import os
 import cv2
 import skimage.io
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 import zipfile
 import numpy as np
 
-TRAIN = '../input/prostate-cancer-grade-assessment/train_images/'
-MASKS = '../input/prostate-cancer-grade-assessment/train_label_masks/'
-OUT_TRAIN = 'train.zip'
-OUT_MASKS = 'masks.zip'
+ROOT = os.path.realpath("../input/prostate-cancer-grade-assessment")
+TRAIN = os.path.join(ROOT, 'train_images')
+SAVE_DIR = os.path.join(ROOT, 'iafoss_train')
 sz = 128
 N = 16
 
-def tile(img, mask):
+
+def tile(img):
     result = []
     shape = img.shape
     pad0,pad1 = (sz - shape[0]%sz)%sz, (sz - shape[1]%sz)%sz
     img = np.pad(img,[[pad0//2,pad0-pad0//2],[pad1//2,pad1-pad1//2],[0,0]],
                 constant_values=255)
-    mask = np.pad(mask,[[pad0//2,pad0-pad0//2],[pad1//2,pad1-pad1//2],[0,0]],
-                constant_values=0)
     img = img.reshape(img.shape[0]//sz,sz,img.shape[1]//sz,sz,3)
     img = img.transpose(0,2,1,3,4).reshape(-1,sz,sz,3)
-    mask = mask.reshape(mask.shape[0]//sz,sz,mask.shape[1]//sz,sz,3)
-    mask = mask.transpose(0,2,1,3,4).reshape(-1,sz,sz,3)
     if len(img) < N:
-        mask = np.pad(mask,[[0,N-len(img)],[0,0],[0,0],[0,0]],constant_values=0)
         img = np.pad(img,[[0,N-len(img)],[0,0],[0,0],[0,0]],constant_values=255)
     idxs = np.argsort(img.reshape(img.shape[0],-1).sum(-1))[:N]
     img = img[idxs]
-    mask = mask[idxs]
     for i in range(len(img)):
-        result.append({'img':img[i], 'mask':mask[i], 'idx':i})
+        result.append({'img':img[i], 'idx':i})
     return result
 
-names = [name[:-10] for name in os.listdir(MASKS)]
-with zipfile.ZipFile(OUT_TRAIN, 'w') as img_out,\
- zipfile.ZipFile(OUT_MASKS, 'w') as mask_out:
-    for name in tqdm(names):
-        print(name)
-        img = skimage.io.MultiImage(os.path.join(TRAIN,name+'.tiff'))[-1]
-        mask = skimage.io.MultiImage(os.path.join(MASKS,name+'_mask.tiff'))[-1]
-        tiles = tile(img,mask)
-        concatenated_img = np.zeros((sz*4, sz*4, 3))
-        concatenated_mask = np.zeros((sz*4, sz*4))
-        for i, t in enumerate(tiles):
-            img,mask,idx = t['img'],t['mask'],t['idx']
-            concatenated_img[128*(i//4):128*(i//4 + 1), 128*(i%4):128*(i%4 + 1)] = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            concatenated_mask[128*(i//4):128*(i//4 + 1), 128*(i%4):128*(i%4 + 1)] = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
-            #if read with PIL RGB turns into BGR
-        concatenated_img = cv2.imencode('.png',concatenated_img)[1]
-        img_out.writestr(f'{name}.png', concatenated_img)
-        concatenated_mask = cv2.imencode('.png', concatenated_mask)[1]
-        mask_out.writestr(f'{name}.png', concatenated_mask)
 
-    img_out.extractall('../input/prostate-cancer-grade-assessment/iafoss')
-    mask_out.extractall('../input/prostate-cancer-grade-assessment/iafoss_mask')
-    
+if not os.path.isdir(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
+names = [name for name in os.listdir(TRAIN)]
+for name in tqdm(names):
+    img = skimage.io.MultiImage(os.path.join(TRAIN,name))[-1]
+    tiles = tile(img)
+    concatenated_img = np.zeros((sz*4, sz*4, 3))
+    for i, t in enumerate(tiles):
+        img,idx = t['img'],t['idx']
+        concatenated_img[128*(i//4):128*(i//4 + 1), 128*(i%4):128*(i%4 + 1)] = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        #if read with PIL RGB turns into BGR
+    cv2.imwrite("{}.png".format(os.path.join(SAVE_DIR, name.split('.')[0])), concatenated_img)
